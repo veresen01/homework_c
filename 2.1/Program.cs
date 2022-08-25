249c41d60bb5bb05c67143d2b954c4e2a89f241a
Yuriy <versen01@yandex.ru>
2022-08-25 22:31:37 +0300

    2.1

---
+++ 2.1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+ 
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+ 
+int a1 = num / 10;
+int a2 = a1 % 10;
+ 
+Console.WriteLine(a2);
