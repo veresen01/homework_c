98412181596dd01dc7291eefb891e6bd04e2ec9c
Yuriy <versen01@yandex.ru>
2022-09-02 22:22:19 +0300

    4.2

---
+++ 4.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+Console.WriteLine("введите число");
+int i = int.Parse(Console.ReadLine());
+int sum = 0;
+while (i >0)
+{
+    int num = i % 10;
+    i = i/10;
+    sum=sum + num;
+
+}
+Console.WriteLine("сумма всех цифр в равна: ");
+Console.WriteLine(sum);
