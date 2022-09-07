6b996140318b150d923b4f6e64aae4619d5d451e
Yuriy <versen01@yandex.ru>
2022-09-07 10:09:38 +0300

    5.3

---
+++ 5.3/Program.cs
@@ -0,0 +1,18 @@
+﻿
+//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+Console.Clear();
+Console.WriteLine("введите длинну массива ");
+int size = int.Parse(Console.ReadLine());
+int[] rand = new int[size];
+ 
+ 
+for(int i = 0; i < rand.Length; i++){
+    rand[i] = new Random().Next(0, 100);
+    Console.Write(rand[i]+" ");
+}
+Console.WriteLine();
+int maxValue = rand.Max<int>();
+ 
+int minValue = rand.Min<int>();
+ 
+Console.WriteLine($"Разница между максимальным и минимальным элементом: {maxValue-minValue}");
