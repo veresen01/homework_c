0bd6fcc53febfda5cc6b517b904de491fc0e26a5
Yuriy <versen01@yandex.ru>
2022-09-18 21:33:11 +0300

    9.1

---
+++ 9.1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. 
+//Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+Console.WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число M:");
+int numberN = int.Parse(Console.ReadLine());
+
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
+GapNumberSum(numberM, numberN, 0);
